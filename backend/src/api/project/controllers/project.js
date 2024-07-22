2db5f995c71c33403be56ec21a1aefdcace17949
adautovjr <26850257+adautovjr@users.noreply.github.com>
2024-07-22 08:43:39 -0300

    fix: Created correct content-types

--- backend/src/api/project/controllers/project.js
+++ backend/src/api/project/controllers/project.js
@@ -1,9 +1,9 @@
 'use strict';
 
 /**
- *  article controller
+ * project controller
  */
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::article.article');
+module.exports = createCoreController('api::project.project');
