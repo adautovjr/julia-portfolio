7e0945991dc2dd3ae7a96dcc547df05c72807dc7
adautovjr <26850257+adautovjr@users.noreply.github.com>
2024-07-27 16:06:55 -0300

    fix: Fix CORS directives for production

--- backend/config/middlewares.js
+++ backend/config/middlewares.js
@@ -1,4 +1,7 @@
-module.exports = ({ env }) => [
+module.exports = ({ env }) => {
+  console.log("🚀 ~ env:", env)
+
+  return [
   'strapi::errors',
   {
     name: 'strapi::security',
@@ -23,11 +26,19 @@ module.exports = ({ env }) => [
       }
     },
   },
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: [`${env('STRAPI_ADMIN_BACKEND_URL')}`],
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
+      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
+      keepHeaderOnError: true,
+    },
+  },,
   'strapi::poweredBy',
   'strapi::logger',
   'strapi::query',
   'strapi::body',
   'strapi::favicon',
   'strapi::public',
-];
+]};
